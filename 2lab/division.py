41ea6c670dc06c82e6149a77f82675f34202533a
Gorlov Andrey <a.b.gorlov@gmail.com>
2023-03-10 13:21:48 +0300

    apd div

--- 2lab/division.py
+++ 2lab/division.py
@@ -1,20 +1,22 @@
 def division_alg(keys):
-    length=len(keys)
-    while True: #тут мы ищем простое
-        if prime(length):
+    M=len(keys)
+    while True: 
+        if prime(M):
             break
-        length+=1
+        M+=1
     
-    return [x % length for x in keys]
+    return [i % M for i in keys]
 
 
-def prime(lenght): #на сайте написано, что число должно быть простым
+def prime(M): 
     d=2
-    while d*d<=lenght:
-        if lenght % d ==0:
+    while d*d<=M:
+        if M % d ==0:
             return False
         d+=1
     return True
-    
-keys=[25,19,7,34,16,61,44,81]
+
+line='hello world!'
+keys=[ord(i) for i in line]
+print(keys)
 print(division_alg(keys))
