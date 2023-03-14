032fda045604bca9f74c351535fdc60173cd6c3b
Gorlov Andrey <a.b.gorlov@gmail.com>
2023-03-14 10:52:18 +0300

    Create mytry.py

---
+++ 2lab/mytry.py
@@ -0,0 +1,27 @@
+def hashing(a):
+    
+    s=0
+    m=256
+    for i in a:
+        s+=ord(i)
+    hash=s%m
+    
+    return finding(hash,a)
+
+
+def finding(hash,a):
+    while hashtable[hash]:
+        hash=(hash+1)%256
+    hashtable[hash]=a
+    return hash
+    
+
+m=256
+hashtable=[0]*m
+
+print(hashing('ABCDEF'))
+print(hashing('ABCDFE'))
+print(hashing('DDDCCC'))
+print(hashtable)
+
+line=hashing('ABCDEF')
