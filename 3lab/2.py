4e9ec3fd4cba409575c7d5e98f3c5f288f419409
drachiovaelena <Malenago.kay@yandex.ru>
2023-03-22 11:28:32 +0300

    new

--- 3lab/2.py
+++ 3lab/2.py
@@ -1,49 +1,52 @@
-M = 4  # кол-о заходов
-K = 20  # вместимось рюкзака вора (кг)
 
-# спискок экспонатов: название, вес (кг), цена
-prod = {
-    'голова зверя': [12, 10_000],
-    'китайская статуя': [20, 5_000],
-    'ложка царя': [2, 20_000],
-    'картина': [13, 9_000],
-    'чемодан': [40, 6_000],
+M=4 #кол-о заходов
+K=20 #вместимось рюкзака вора (кг)
+
+#спискок экспонатов: название, вес (кг), цена
+prod={
+    'голова зверя':[19,20_000],
+    'китайская статуя':[20,5_000],
+    'ложка царя':[2,20_000],
+    'картина':[13,9_000],
+    'чемодан':[19,21_000],
 }
 
-N = len(prod)  # кол-о экспонатов
+N=len(prod) #кол-о экспонатов
 
-# вичислим ценность экспонатов (цена/кг)
-prod_val = {}
+#вичислим ценность экспонатов (цена/кг)
+prod_val={}
 for i in prod.keys():
-    val = prod[i][1] / prod[i][0]
-    prod_val[i] = val
+    val=prod[i][1]/prod[i][0]
+    prod_val[i]=val
 
-# отсортируем по убыванию ценности
+#отсортируем по убыванию ценности
 prod_sort = sorted(prod_val.items(), key=lambda x: x[1], reverse=True)
-dict(prod_sort)
+prod_sort=dict(prod_sort)
 
-# используем жадный алгоритм, выбираем товары
-mine = {}  # храним товары, вес и стоимость
+#используем жадный алгоритм, выбираем товары
+mine={} #храним товары, вес и стоимость
 
+print(prod_sort)
 for k in range(M):
-    weight = 0  # текущий вес захода
-    count = 0  # счетчик, чтобы не уходить в бесконечный цикл
+    weight=0 #текущий вес захода
+    count=0 #счетчик, чтобы не уходить в бесконечный цикл
 
-    while weight < K and len(prod_sort) != 0 and count == 0:
-        pop_num = []  # индексы товаров, которые нужно удалить (ибо их взяли)
-        for i in range(len(prod_sort)):
-            count = 1
-            # проверка, добавление, удаление
-            if prod[prod_sort[i][0]][0] + weight <= K:
-                mine[prod_sort[i][0]] = prod[prod_sort[i][0]]
-                weight += prod[prod_sort[i][0]][0]
-                pop_num.append(i)
+    while weight<K and len(prod_sort)!=0 and count==0:
+        pop_n=[] #товары, которые нужно удалить (ибо их взяли)
+        for i in prod_sort.keys():
+            count=1
+            #проверка, добавление, удаление
+            if prod[i][0]+weight<=K:
+                mine[i]=prod[i]
+                weight+=prod[i][0]
+                pop_n.append(i)
 
-        print(f"Взяли - {mine}, Вес захода - {weight}, Номер захода - {k + 1}")
-        for num in pop_num:
-            prod_sort.pop(num)
+        print(f"Взяли - {mine}, Вес захода - {weight}, Номер захода - {k+1}")
 
-# вывод
+        for n in pop_n:
+            del prod_sort[n]
+
+#вывод
 print('\n'
       'Итого: '
       f"{mine}\n"
@@ -52,4 +55,3 @@ print('\n'
       'Вес: '
       f"{sum([prod[i][0] for i in mine.keys()])} кг"
       )
-
